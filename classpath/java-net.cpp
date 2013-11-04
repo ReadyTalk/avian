b71aca03a12e4170a367cfcec96455e27bf656e4
Ilya Mizus <bigfatbrowncat@gmail.com>
2013-11-05 00:07:43 +0300

    This version simply works on Windows
    Now it works on OS X
    
    Small improvements
    
    Added bufferes I/O
    
    Small fixes
    
    Added getHostName() function

--- classpath/java-net.cpp
+++ classpath/java-net.cpp
@@ -8,18 +8,35 @@
    There is NO WARRANTY for this software.  See license.txt for
    details. */
 
+
 #include "jni.h"
 #include "jni-util.h"
 
 #ifdef PLATFORM_WINDOWS
 #  include <winsock2.h>
-#  define ONLY_ON_WINDOWS(x) x
+
+#  define ONLY_ON_WINDOWS(x)	x
 #else
 #  include <netdb.h>
 #  include <sys/socket.h>
 #  include <netinet/in.h>
+#  include <unistd.h>
+
 #  define ONLY_ON_WINDOWS(x)
+#  define SOCKET				int
+#  define INVALID_SOCKET		-1
+#  define SOCKET_ERROR			-1
+#  define closesocket(x)		close(x)
 #endif
+
+int last_socket_error() {
+#ifdef PLATFORM_WINDOWS
+		int error = WSAGetLastError();
+#else
+		int error = errno;
+#endif
+		return error;
+}
 
 extern "C" JNIEXPORT void JNICALL
 Java_java_net_Socket_init(JNIEnv* ONLY_ON_WINDOWS(e), jclass)
@@ -38,6 +55,133 @@ Java_java_net_Socket_init(JNIEnv* ONLY_ON_WINDOWS(e), jclass)
 #endif
 }
 
+extern "C" JNIEXPORT SOCKET JNICALL
+Java_java_net_Socket_create(JNIEnv* e, jclass) {
+	SOCKET sock;
+	if (INVALID_SOCKET == (sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))) {
+		char buf[255];
+		sprintf(buf, "Can't create a socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+		return 0;	// This doesn't matter cause we have risen an exception
+	}
+	return sock;
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_connect(JNIEnv* e, jclass, SOCKET sock, long addr, short port) {
+	sockaddr_in adr;
+	adr.sin_family = AF_INET;
+#ifdef PLATFORM_WINDOWS
+	adr.sin_addr.S_un.S_addr = htonl(addr);
+#else
+	adr.sin_addr.s_addr = htonl(addr);
+#endif
+	adr.sin_port = htons (port);
+
+	if (SOCKET_ERROR == connect(sock, (sockaddr* )&adr, sizeof(adr)))
+	{
+		char buf[255];
+		sprintf(buf, "Can't connect a socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+		return;
+	}
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_bind(JNIEnv* e, jclass, SOCKET sock, long addr, short port) {
+	sockaddr_in adr;
+	adr.sin_family = AF_INET;
+#ifdef PLATFORM_WINDOWS
+	adr.sin_addr.S_un.S_addr = htonl(addr);
+#else
+	adr.sin_addr.s_addr = htonl(addr);
+#endif
+	adr.sin_port = htons (port);
+
+	if (SOCKET_ERROR == bind(sock, (sockaddr* )&adr, sizeof(adr)))
+	{
+		char buf[255];
+		sprintf(buf, "Can't bind a socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+		return;
+	}
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_send(JNIEnv* e, jclass, SOCKET sock, const char* buff_ptr, int buff_size) {
+	if (SOCKET_ERROR == send(sock, buff_ptr, buff_size, 0)) {
+		char buf[255];
+		sprintf(buf, "Can't send data through the socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+		return;
+	}
+}
+
+extern "C" JNIEXPORT int JNICALL
+Java_java_net_Socket_recv(JNIEnv* e, jclass, SOCKET sock, char* buff_ptr, int buff_size) {
+	int length = recv(sock, buff_ptr, buff_size, 0);
+	if (SOCKET_ERROR == length) {
+		char buf[255];
+		sprintf(buf, "Can't receive data through the socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+		return 0;	// This doesn't matter cause we have risen an exception
+	}
+	return length;
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_closeAbortive(JNIEnv* e, jclass, SOCKET sock) {
+	if (SOCKET_ERROR == closesocket(sock)) {
+		char buf[255];
+		sprintf(buf, "Can't close the socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+	}
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_closeGraceful(JNIEnv* e, jclass, SOCKET sock, int how) {
+	if (SOCKET_ERROR == shutdown(sock, how)) {
+		char buf[255];
+		sprintf(buf, "Can't shutdown the socket. System error: %d", last_socket_error());
+		throwNew(e, "java/io/IOException", buf);
+	}
+}
+
+extern "C" JNIEXPORT char* JNICALL
+Java_java_net_Socket_allocateBuffer(JNIEnv*, jclass, int buf_size) {
+	return (char*)malloc(buf_size);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_freeBuffer(JNIEnv*, jclass, char* buf_ptr) {
+	free(buf_ptr);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_copyBufferFromNative(JNIEnv* e, jclass, char* source_ptr, int source_size, jbyteArray target, int start_pos) {
+	if (source_size > e->GetArrayLength(target) - start_pos) {
+		throwNew(e, "java/lang/RuntimeException", "The source buffer is greater than the target array");
+		return;
+	}
+	jbyte* targetElements = e->GetByteArrayElements(target, NULL);
+	memcpy(targetElements + start_pos, source_ptr, source_size);
+	e->ReleaseByteArrayElements(target, targetElements, (int)0);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_java_net_Socket_copyBufferToNative(JNIEnv* e, jclass, jbyteArray source, int start_pos, int target_size, char* target) {
+	int source_size = e->GetArrayLength(source);
+	if (source_size - start_pos > target_size) {
+		throwNew(e, "java/lang/RuntimeException", "The source array is greater than the target buffer");
+		return;
+	}
+	jbyte* sourceElements = e->GetByteArrayElements(source, NULL);
+	memcpy(target, sourceElements + start_pos, target_size);
+	e->ReleaseByteArrayElements(source, sourceElements, JNI_ABORT);
+}
+
+
 extern "C" JNIEXPORT jint JNICALL
 Java_java_net_InetAddress_ipv4AddressForName(JNIEnv* e,
                                              jclass,
@@ -49,7 +193,7 @@ Java_java_net_InetAddress_ipv4AddressForName(JNIEnv* e,
     hostent* host = gethostbyname(chars);
     e->ReleaseStringUTFChars(name, chars);
     if (host) {
-      return htonl(reinterpret_cast<in_addr*>(host->h_addr_list[0])->s_addr);
+      return ntohl(reinterpret_cast<in_addr*>(host->h_addr_list[0])->s_addr);
     } else {
       fprintf(stderr, "trouble %d\n", WSAGetLastError());
     }
@@ -67,7 +211,7 @@ Java_java_net_InetAddress_ipv4AddressForName(JNIEnv* e,
     if (r != 0) {
       address = 0;
     } else {
-      address = htonl
+      address = ntohl
         (reinterpret_cast<sockaddr_in*>(result->ai_addr)->sin_addr.s_addr);
 
       freeaddrinfo(result);
@@ -78,3 +222,4 @@ Java_java_net_InetAddress_ipv4AddressForName(JNIEnv* e,
   }
   return 0;
 }
+
--- classpath/java/net/Socket.java
+++ classpath/java/net/Socket.java
@@ -10,10 +10,208 @@
 
 package java.net;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public abstract class Socket {
-  public static native void init() throws IOException;
+public class Socket implements Closeable, AutoCloseable {
 
-  public abstract void setTcpNoDelay(boolean on) throws SocketException;
+	private static final int SD_RECEIVE = 0x00;
+	private static final int SD_SEND = 0x01;
+	private static final int SD_BOTH = 0x02;
+	
+	private static final int BUFFER_SIZE = 65535;
+	
+	/**
+	 * This method is called from all routines that depend on winsock in windows,
+	 * so it has public visibility
+	 * @throws IOException
+	 */
+	public static native void init() throws IOException;
+	
+	/**
+	 * Creates the native socket object
+	 * @return Handle to the native object
+	 * @throws IOException
+	 */
+	private static native /* SOCKET */long create() throws IOException;
+	
+	/**
+	 * Connects the native socket object to an address:port
+	 * @param sock Native socket handler
+	 * @param addr Address to connect to
+	 * @param port Port to connect to
+	 * @throws IOException
+	 */
+	private static native void connect(/* SOCKET */long sock, long addr, short port) throws IOException;
+	private static native void bind(/* SOCKET */long sock, long addr, short port) throws IOException;
+
+	private static native void send(/* SOCKET */long sock, /* char* */long buff_ptr, int buff_size) throws IOException;
+	private static native int recv(/* SOCKET */long sock, /* char* */long buff_ptr, int buff_size) throws IOException;
+	
+	private static native void closeAbortive(/* SOCKET */long sock);
+	private static native void closeGraceful(/* SOCKET */long sock, int how);
+	
+	private static native /* char* */long allocateBuffer(int buf_size);
+	private static native void freeBuffer(/* char* */long buf_ptr);
+	private static native void copyBufferFromNative(long source_ptr, int source_size, byte[] target, int start_pos);
+	private static native void copyBufferToNative(byte[] source, int start_pos, int target_size, long target);
+	
+	private class SocketInputStream extends InputStream {
+
+		private long buf_ptr;
+		
+		public SocketInputStream() {
+			buf_ptr = allocateBuffer(BUFFER_SIZE);
+		}
+		
+		@Override
+		public void close() throws IOException {
+			if (buf_ptr != 0) {
+				closeGraceful(sock, SD_RECEIVE);
+				freeBuffer(buf_ptr);
+				buf_ptr = 0;
+			}
+			super.close();
+		}
+		
+		@Override
+		protected void finalize() throws Throwable {
+			close();
+			super.finalize();
+		}
+		
+		@Override
+		public int read() throws IOException {
+			int size = recv(sock, buf_ptr, 1);
+			if (size == 0) {
+				return -1;
+			}
+			byte[] res = new byte[1];
+			copyBufferFromNative(buf_ptr, 1, res, 0);
+			return res[0];
+		}
+		
+		@Override
+		public int read(byte[] buffer) throws IOException {
+			int fullSize = buffer.length;
+			int index = 0;
+			int size;
+			do {
+				size = recv(sock, buf_ptr, Math.min(fullSize, Socket.BUFFER_SIZE));
+				fullSize -= size;
+				copyBufferFromNative(buf_ptr, size, buffer, index);
+				index += size;
+			} while (fullSize != 0 && size != 0);
+			return index;
+		}
+
+		
+	}
+	
+	private class SocketOutputStream extends OutputStream {
+
+		private long buf_ptr;
+		
+		public SocketOutputStream() {
+			buf_ptr = allocateBuffer(BUFFER_SIZE);
+		}
+		
+		@Override
+		public void close() throws IOException {
+			if (buf_ptr != 0) {
+				closeGraceful(sock, SD_SEND);
+				freeBuffer(buf_ptr);
+				buf_ptr = 0;
+			}
+			super.close();
+		}
+		
+		@Override
+		protected void finalize() throws Throwable {
+			close();
+			super.finalize();
+		}
+		
+		@Override
+		public void write(int c) throws IOException {
+			byte[] res = new byte[1];
+			res[0] = (byte)c;
+			copyBufferToNative(res, 0, 1, buf_ptr);
+			send(sock, buf_ptr, 1);
+		}
+		
+		@Override
+		public void write(byte[] buffer) throws IOException {
+			int fullSize = buffer.length;
+			int index = 0;
+			int size;
+			do {
+				size = Math.min(fullSize, Socket.BUFFER_SIZE);
+				copyBufferToNative(buffer, index, size, buf_ptr);
+				send(sock, buf_ptr, size);
+				fullSize -= size;
+				index += size;
+			} while (fullSize != 0 && size != 0);
+		}
+
+	}
+
+	private long sock;
+	private SocketInputStream inputStream;
+	private SocketOutputStream outputStream;
+	
+	public Socket() throws IOException {
+		Socket.init();
+		sock = create();
+		inputStream = new SocketInputStream();
+		outputStream = new SocketOutputStream();
+	}
+	
+	public SocketInputStream getInputStream() {
+		return inputStream;
+	}
+	
+	public SocketOutputStream getOutputStream() {
+		return outputStream;
+	}
+	
+	public Socket(InetAddress address, int port) throws IOException {
+		this();
+		connect(sock, address.getRawAddress(), (short)port);
+	}
+	
+	public Socket(String host, int port) throws UnknownHostException, IOException {
+		this(InetAddress.getByName(host), port);
+	}
+	
+	public void bind(SocketAddress bindpoint) throws IOException {
+		if (bindpoint instanceof InetSocketAddress) {
+			InetSocketAddress inetBindpoint = (InetSocketAddress)bindpoint;
+			bind(sock, inetBindpoint.getAddress().getRawAddress(), (short) inetBindpoint.getPort());
+		}
+	}
+	
+	public void setTcpNoDelay(boolean on) throws SocketException {}
+
+	@Override
+	public void close() throws IOException {
+		closeGraceful(sock, SD_BOTH);
+	}
+	
+	public void shutdownInput() throws IOException {
+		inputStream.close();
+	}
+	
+	public void shutdownOutput() throws IOException {
+		outputStream.close();
+	}
+	
+	
+	@Override
+	protected void finalize() throws Throwable {
+		close();
+		super.finalize();
+	}
 }
