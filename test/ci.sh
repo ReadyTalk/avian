987badd0b54120674c147f6f7e14895853b5dae9
Joel Dice <joel.dice@gmail.com>
2014-02-24 14:25:21 -0700

    test both 32-bit and 64-bit arches in ci.sh

--- test/ci.sh
+++ test/ci.sh
@@ -2,12 +2,20 @@
 
 set -e
 
-make ${flags} jdk-test
-make ${flags} test
-make ${flags} mode=debug test
-make ${flags} process=interpret test
+run() {
+  echo "${@}"
+  ${@}
+}
+
+run make ${flags} jdk-test
+
+for arch in i386 x86_64; do
+  run make arch=${arch} ${flags} test
+  run make arch=${arch} ${flags} mode=debug test
+  run make arch=${arch} ${flags} process=interpret test
 # bootimage and openjdk builds without openjdk-src don't work:
-if [ -z "${openjdk}" ]; then
-  make ${flags} bootimage=true test
-fi
-make ${flags} tails=true continuations=true test
+  if [ -z "${openjdk}" ]; then
+    run make arch=${arch} ${flags} bootimage=true test
+  fi
+  run make arch=${arch} ${flags} tails=true continuations=true test
+done
