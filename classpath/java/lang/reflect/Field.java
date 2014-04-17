59fcefcabf0b753af694bd707216aee5fbc91e64
Joel Dice <joel.dice@gmail.com>
2014-04-17 11:32:37 -0600

    backport fixes for NPEs in Field.getAnnotation{s}

--- classpath/java/lang/reflect/Field.java
+++ classpath/java/lang/reflect/Field.java
@@ -225,7 +225,7 @@ public class Field<T> extends AccessibleObject {
   }
 
   public <T extends Annotation> T getAnnotation(Class<T> class_) {
-    if (vmField.addendum.annotationTable != null) {
+    if (vmField.addendum != null && vmField.addendum.annotationTable != null) {
       Object[] table = (Object[]) vmField.addendum.annotationTable;
       for (int i = 0; i < table.length; ++i) {
         Object[] a = (Object[]) table[i];
@@ -238,7 +238,7 @@ public class Field<T> extends AccessibleObject {
   }
 
   public Annotation[] getAnnotations() {
-    if (vmField.addendum.annotationTable != null) {
+    if (vmField.addendum != null && vmField.addendum.annotationTable != null) {
       Object[] table = (Object[]) vmField.addendum.annotationTable;
       Annotation[] array = new Annotation[table.length];
       for (int i = 0; i < table.length; ++i) {
